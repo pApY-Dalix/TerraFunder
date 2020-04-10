d009d7c46c579182a5b72c7c666825cdc37a4194
pApY-Dalix <dalix.playgamer@gmail.com>
2020-04-10 15:27:05 +0200

    GuiEvent Update

--- src/fr/terrafunder/event/GuiEvent.java
+++ src/fr/terrafunder/event/GuiEvent.java
@@ -30,30 +30,31 @@ public class GuiEvent implements Listener {
         Player player = event.getPlayer();
         Action action = event.getAction();
         ItemStack it = event.getItem();
-        if (it == null) return;
-
+        if (it == null)
+        {
+            return;
+        }
         if (it.getType() == Material.CHEST && it.hasItemMeta() && it.getItemMeta().hasDisplayName() && it.getItemMeta().getDisplayName().equalsIgnoreCase("§6Sélecteur de team"))
         {
             Inventory inv = Bukkit.createInventory(null, 45, "§6Sélecteur de team");
-            ItemStack glas = new ItemStack(Material.STAINED_GLASS_PANE,1);
+            ItemStack glass = new ItemStack(Material.STAINED_GLASS_PANE,1);
             int i = 0;
             while (i < 10) {
-                inv.setItem(i, glas);
+                inv.setItem(i, glass);
                 i++;
             }
-            inv.setItem(17, glas);
-            inv.setItem(18, glas);
-            inv.setItem(26, glas);
-            inv.setItem(27, glas);
+            inv.setItem(17, glass);
+            inv.setItem(18, glass);
+            inv.setItem(26, glass);
+            inv.setItem(27, glass);
             int j = 35;
             while (j < 45 )
             {
-                inv.setItem(j, glas);
+                inv.setItem(j, glass);
                 j++;
             }
             inv.setItem(20, choseteam());
             player.openInventory(inv);
-
         }
     }
 }
